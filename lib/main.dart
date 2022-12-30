67ae19902f2aec36438a878c5c663d550e805bf2
hinut <si19003@edu.aomori-u.ac.jp>
2022-12-30 14:42:23 +0900

    インデント修正

--- lib/main.dart
+++ lib/main.dart
@@ -70,12 +70,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -95,7 +89,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         dataSource: GoogleDataSource(events: snapshot.data),
                         monthViewSettings: MonthViewSettings(
                             appointmentDisplayMode:
-                            MonthAppointmentDisplayMode.appointment),
+                            MonthAppointmentDisplayMode.appointment
+                        ),
                       ),
                     ),
                     snapshot.data != null
@@ -104,7 +99,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: CircularProgressIndicator(),
                     )
                   ],
-                ));
+                )
+            );
           },
         ),
       ),
